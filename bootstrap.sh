20038bf99ee48efa51978288e832a9340004e0ed
jdnl <jdonlin@gmail.com>
2023-07-29 14:42:49 -0700

    unattended installs for bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,12 +2,12 @@
 
 install_gpg() {
   # gpg might be installed by gnome-core/git already
-  sudo apt install gpg
+  sudo apt install -y gpg
 }
 
 install_gnome() {
   # Install gnome with minimal excess bloat
-  sudo apt install --no-install-recommends gnome-session gdm3
+  sudo apt install -y --no-install-recommends gnome-session gdm3
 }
 
 install_regolith_desktop() {
@@ -16,12 +16,12 @@ install_regolith_desktop() {
   echo deb "[arch=amd64 signed-by=/usr/share/keyrings/regolith-archive-keyring.gpg] https://regolith-desktop.org/testing-debian-bookworm-amd64 bookworm main" | \
   sudo tee /etc/apt/sources.list.d/regolith.list
   sudo apt update
-  sudo apt install regolith-desktop
+  sudo apt install -y regolith-desktop
 }
 
 install_ansible() {
   # Install ansible
-  sudo apt install ansible
+  sudo apt install -y ansible
 }
 
 main() {
